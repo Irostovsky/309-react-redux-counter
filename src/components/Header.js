d2068f36e0a79dc4398b614fb74fc32dfb3ba4a7
Irostovsky <ivan.rostovsky@gmail.com>
2025-04-04 17:37:45 +0300

    Naive login logout added.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import classes from "./Header.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store";
 
 const Header = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -16,7 +22,7 @@ const Header = () => {
               <a href="/">My Sales</a>
             </li>
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           </ul>
         </nav>
